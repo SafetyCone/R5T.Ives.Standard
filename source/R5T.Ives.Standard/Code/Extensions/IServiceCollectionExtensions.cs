8cdfc8e0eea37b75d06922f13e00234a46bbb1f4
MinexAutomation <minex.automation@gmail.com>
2019-12-22 00:10:36 -0800

    Added extensions get the configuration name from an IServiceCollection and an IServiceProvider.

---
+++ source/R5T.Ives.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia.Extensions;
+
+
+namespace R5T.Ives.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static string GetConfigurationName(this IServiceCollection services)
+        {
+            var intermediateServiceProvider = services.BuildIntermediateServiceProvider();
+
+            var configurationName = intermediateServiceProvider.GetConfigurationName();
+            return configurationName;
+        }
+    }
+}
---
+++ source/R5T.Ives.Standard/Code/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Ives.Standard
+{
+    public static class IServiceProviderExtensions
+    {
+        public static string GetConfigurationName(this IServiceProvider serviceProvider)
+        {
+            var configurationNameProvider = serviceProvider.GetRequiredService<IConfigurationNameProvider>();
+
+            var configurationName = configurationNameProvider.GetConfigurationName();
+            return configurationName;
+        }
+    }
+}
